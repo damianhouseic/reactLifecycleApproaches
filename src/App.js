d424e465717e3d21e3d15f5656a8b7130cb6b498
Damian House <dhouse@icontact.com>
2019-11-18 14:41:22 -0500

    Added prettier and eslint for funsies

--- src/App.js
+++ src/App.js
@@ -1,70 +1,58 @@
-import React from "react";
-import "./App.css";
-import ContactData from "./component/lifecycle/ContactData";
-import MessageData from "./component/lifecycle/MessageData";
-import ContactDataHOC from "./component/higherOrder/ContactDataHOC";
-import MessageDataHOC from "./component/higherOrder/MessageDataHOC";
-import ContactDataWithHooks from "./component/future/ContactDataWithHooks";
-import MessageDataWithHooks from "./component/future/MessageDataWithHooks";
-import { Card, Row, Col } from "react-materialize";
+import React from 'react';
+import './App.css';
+import ContactData from './component/lifecycle/ContactData';
+import MessageData from './component/lifecycle/MessageData';
+import ContactDataHOC from './component/higherOrder/ContactDataHOC';
+import MessageDataHOC from './component/higherOrder/MessageDataHOC';
+import ContactDataWithHooks from './component/future/ContactDataWithHooks';
+import MessageDataWithHooks from './component/future/MessageDataWithHooks';
+import { Card, Row, Col } from 'react-materialize';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <Row>
-          <Col l={4} m={6} s={12}>
-            <Card
-              className="blue-grey lighten-5"
-              textClassName="black-text"
-              title="Legacy Class Lifecycle"
-            >
-              <Row>
-                <Col l={6} className="center-align">
-                  <ContactData />
-                </Col>
-                <Col l={6} className="center-align">
-                  <MessageData />
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-          <Col l={4} m={6} s={12}>
-            <Card
-              className="blue-grey lighten-5"
-              textClassName="black-text"
-              title="Higher Order Component"
-            >
-              <Row>
-                <Col l={6} className="center-align">
-                  <ContactDataHOC />
-                </Col>
-                <Col l={6} className="center-align">
-                  <MessageDataHOC />
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-          <Col l={4} m={6} s={12}>
-            <Card
-              className="blue-grey lighten-5"
-              textClassName="black-text"
-              title="Hooks"
-            >
-              <Row>
-                <Col l={6} className="center-align">
-                  <ContactDataWithHooks />
-                </Col>
-                <Col l={6} className="center-align">
-                  <MessageDataWithHooks />
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <Row>
+                    <Col l={4} m={6} s={12}>
+                        <Card className="blue-grey lighten-5" textClassName="black-text" title="Legacy Class Lifecycle">
+                            <Row>
+                                <Col l={6} className="center-align">
+                                    <ContactData />
+                                </Col>
+                                <Col l={6} className="center-align">
+                                    <MessageData />
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                    <Col l={4} m={6} s={12}>
+                        <Card className="blue-grey lighten-5" textClassName="black-text" title="Higher Order Component">
+                            <Row>
+                                <Col l={6} className="center-align">
+                                    <ContactDataHOC />
+                                </Col>
+                                <Col l={6} className="center-align">
+                                    <MessageDataHOC />
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                    <Col l={4} m={6} s={12}>
+                        <Card className="blue-grey lighten-5" textClassName="black-text" title="Hooks">
+                            <Row>
+                                <Col l={6} className="center-align">
+                                    <ContactDataWithHooks />
+                                </Col>
+                                <Col l={6} className="center-align">
+                                    <MessageDataWithHooks />
+                                </Col>
+                            </Row>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/api/data.js
+++ src/api/data.js
@@ -1,22 +1,30 @@
 export class Api {
-  static async getContact() {
-    const delay = t => new Promise(resolve => setTimeout(resolve, t));
+    static async getContact() {
+        const delay = t => new Promise(resolve => setTimeout(resolve, t));
 
-    return await delay(Math.random() * 10000).then(() => {return {name: "John", email: "john@gmail.com"}});
-  }
-  static async getOtherContact() {
-    const delay = t => new Promise(resolve => setTimeout(resolve, t));
+        return await delay(Math.random() * 10000).then(() => {
+            return { name: 'John', email: 'john@gmail.com' };
+        });
+    }
+    static async getOtherContact() {
+        const delay = t => new Promise(resolve => setTimeout(resolve, t));
 
-    return await delay(Math.random() * 10000).then(() => {return {name: "Damian", email: "damian@gmail.com"}});
-  }
-  static async getMessage() {
-    const delay = t => new Promise(resolve => setTimeout(resolve, t));
-    const random = Math.floor(Math.random() * 10000);
-    return await delay(random).then(() => {return {subject: "Black Friday Is Here!", recipients: random}});
-  }
-  static async getOtherMessage() {
-    const delay = t => new Promise(resolve => setTimeout(resolve, t));
-    const random = Math.floor(Math.random() * 10000);
-    return await delay(random).then(() => {return {subject: "Christmas Sales!", recipients: random}});
-  }
+        return await delay(Math.random() * 10000).then(() => {
+            return { name: 'Damian', email: 'damian@gmail.com' };
+        });
+    }
+    static async getMessage() {
+        const delay = t => new Promise(resolve => setTimeout(resolve, t));
+        const random = Math.floor(Math.random() * 10000);
+        return await delay(random).then(() => {
+            return { subject: 'Black Friday Is Here!', recipients: random };
+        });
+    }
+    static async getOtherMessage() {
+        const delay = t => new Promise(resolve => setTimeout(resolve, t));
+        const random = Math.floor(Math.random() * 10000);
+        return await delay(random).then(() => {
+            return { subject: 'Christmas Sales!', recipients: random };
+        });
+    }
 }
